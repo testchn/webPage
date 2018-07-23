386d93de2b77fe199bee43b873fc3609e89ebba0
testchn <41469754+testchn@users.noreply.github.com>
2018-07-23 15:12:57 +0530

    Update UAT.php

--- UAT.php
+++ UAT.php
@@ -1,18 +1,15 @@
-<?php
-$cars = array
-  (
-  array("Volvo",22,18),
-  array("BMW",15,13),
-  array("Saab",5,2),
-  array("Land Rover",17,15)
-  );
-    
-for ($row = 0; $row < 4; $row++) {
-  echo "<p><b>Row number $row</b></p>";
-  echo "<ul>";
-  for ($col = 0; $col < 3; $col++) {
-    echo "<li>".$cars[$row][$col]."</li>";
-  }
-  echo "</ul>";
+{ 
+  "adminSiteName": null,
+  "appServicePlanName": "myAppServicePlan",
+  "geoRegion": "West Europe",
+  "hostingEnvironmentProfile": null,
+  "id": "/subscriptions/0000-0000/resourceGroups/myResourceGroup/providers/Microsoft.Web/serverfarms/myAppServicePlan",
+  "kind": "app",
+  "location": "West Europe",
+  "maximumNumberOfWorkers": 1,
+  "name": "myAppServicePlan",
+  < JSON data removed for brevity. >
+  "targetWorkerSizeId": 0,
+  "type": "Microsoft.Web/serverfarms",
+  "workerTierName": null
 }
-?>
